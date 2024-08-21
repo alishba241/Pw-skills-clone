61b40a00337048c812bd7bbe95948067857055cc
alishba 241 <amnav5206@gmail.com>
2024-08-21 11:15:32 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/PW web.html"],
+  theme: {
+    extend: {
+      colors: {
+         customBlue: '#110e41',
+      },
+    },
+  },
+  plugins: [],
+}
+
